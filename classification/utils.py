323e23f04b9ef7e0f7ad7c6d77cd4e1ec191d1cb
steve <Steven_liu1015@outlook.com>
2023-11-08 11:03:24 -0500

    Changes to be committed: 	modified:   classification/utils.py
    Changes not staged for commit:
    	deleted:    classificationNumber/__pycache__/util.cpython-311.pyc
    	deleted:    classificationNumber/minist_train.py
    	deleted:    classificationNumber/util.py
    
    Untracked files:
    	activationFunc/
    	classification_Minist/
    	dataStructure/
    	loss_func/
    	multiClassification_Minist/

--- classification/utils.py
+++ classification/utils.py
@@ -18,6 +18,7 @@ def get_mean_and_std(dataset):
     mean = torch.zeros(3)
     std = torch.zeros(3)
     print('==> Computing mean and std..')
+    
     for inputs, targets in dataloader:
         for i in range(3):
             mean[i] += inputs[:,i,:,:].mean()
